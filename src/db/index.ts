5b16aefdd98f983de1553b19935a93c73f03d971
m2lovric <m2lovric@gmail.com>
2022-01-31 23:40:25 +0100

    Signup setup

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,19 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.GATSBY_API_KEY,
   authDomain: 'blog-ts-gatsby.firebaseapp.com',
   projectId: 'blog-ts-gatsby',
   storageBucket: 'blog-ts-gatsby.appspot.com',
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_APP_ID,
 };
 
 initializeApp(firebaseConfig);
 
 const db = getFirestore();
-const auth = getAuth();
 
-export { db, auth };
+export { db };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,14 @@ import { useEffect, useState } from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
 
 import Layout from './components/layout';
-import { db, auth } from '../db';
+import { db } from '../db';
 import { collection, onSnapshot } from 'firebase/firestore';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 const IndexPage = () => {
-  const [username, setUsername] = useState(undefined);
-  const [password, setPassword] = useState(undefined);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const auth = getAuth();
 
   useEffect(() => {
     const colRef = collection(db, 'blogs');
@@ -24,15 +25,24 @@ const IndexPage = () => {
   }, []);
 
   const signUpForm = document.querySelector('.signup');
-  signUpForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    createUserWithEmailAndPassword(auth, username, password);
-  });
 
+  signUpForm &&
+    signUpForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      console.log(username, password);
+      createUserWithEmailAndPassword(auth, username, password)
+        .then((userCred) => {
+          const user = userCred.user;
+          console.log(user);
+          setUsername('');
+          setPassword('');
+        })
+        .catch((err) => console.log(err.message));
+    });
   return (
     <Layout pageTitle='Develop...'>
-      <StaticImage src='../images/develop.jpeg' alt='keyboard' />
-      <p>Matteo Lovric 2022</p>
+      <StaticImage src='../images/develop.jpg' alt='keyboard' />
+
       <form className='signup'>
         <input
           type='text'
@@ -46,6 +56,7 @@ const IndexPage = () => {
         />
         <button type='submit'>Signup</button>
       </form>
+      <p>Matteo Lovric 2022</p>
     </Layout>
   );
 };
