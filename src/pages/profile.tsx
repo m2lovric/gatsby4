dccce7eef2933fb45d2091743f02f15f5e569603
m2lovric <m2lovric@gmail.com>
2022-02-04 13:06:39 +0100

    Update profile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,20 +1,19 @@
-import Layout from '../components/layout';
 import * as React from 'react';
+import Layout from '../components/layout';
 import { auth } from '../firebase';
-import { updateProfile } from 'firebase/auth';
 
-const Profile = () => {
+const ProfilePage = () => {
   const currentUser = auth.currentUser;
 
   return (
     <Layout>
       <div>
         <h2>Profile</h2>
-        <p>Name: {currentUser.displayName || 'Name'}</p>
-        <p>Email: {currentUser.email}</p>
+        <p>Name: {'Name' || (currentUser && currentUser.displayName)}</p>
+        <p>Email: {currentUser && currentUser.email}</p>
       </div>
     </Layout>
   );
 };
 
-export default Profile;
+export default ProfilePage;
